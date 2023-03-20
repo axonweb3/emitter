873e83d430e0e0946118592a4b95b21d940eb9bb
felicityin <yinjingping2022@gmail.com>
2023-03-20 12:04:12 +0800

    fix: target address

--- src/emit_data/eth_tx.rs
+++ src/emit_data/eth_tx.rs
@@ -11,8 +11,8 @@ const AXON_URL: &str = "http://localhost:8000";
 const ADDRESS: &str = "0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1";
 const PRIVATE_KEY: &str = "37aa0f893d05914a4def0460c0a984d3611546cfb26924d7a7ca6e0db9950a2d";
 
-pub const IMAGE_CELL_ADDRESS: Address = system_contract_address(0x2);
-pub const CKB_LIGHT_CLIENT_ADDRESS: Address = system_contract_address(0x3);
+pub const IMAGE_CELL_ADDRESS: Address = system_contract_address(0x3);
+pub const CKB_LIGHT_CLIENT_ADDRESS: Address = system_contract_address(0x4);
 
 pub async fn send_eth_tx(data: Vec<u8>, to: Address) -> Result<()> {
     let provider = Provider::<Http>::try_from(AXON_URL)?;
